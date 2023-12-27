7f2592219dcfab767a09112b295736662523fbce
woduf2720 <pjy5602@gmail.com>
2023-12-27 18:10:23 +0900

    db 연결해서 CRUD 테스트 134p까지 완료

---
+++ src/main/java/org/zerock/jdbcex/domain/TodoVO.java
@@ -0,0 +1,18 @@
+package org.zerock.jdbcex.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@Getter
+@Builder
+@ToString
+public class TodoVO {
+
+    private Long tno;
+    private String title;
+    private LocalDate dueDate;
+    private boolean finished;
+}
